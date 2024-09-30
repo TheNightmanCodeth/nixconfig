bcba64cac86c0369b797f8331787e3af7c49903b
Joe Diragi <joseph.diragi@icloud.com>
2024-09-30 19:06:09 +0000

    desktop: avahi

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -142,6 +142,21 @@ in {
       ];
     };
   };
+  
+#### AVAHI
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    nssmdns6 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
 
 #### TIME 
   services.ntp.enable = true;
