05a2233fac5aace26b05c3910860ab9830982d3e
Joe Diragi <joseph.diragi@icloud.com>
2024-10-06 12:07:45 -0400

    thinkpad-X13s: Update kernel

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     swift-59-nixpkgs.url = "github:TheNightmanCodeth/nixpkgs?ref=feat/swift-5.9";
     linux-firmware-main.url = "github:TheNightmanCodeth/linux-firmware-git-flake/main";
-    x13s-nixos.url = "github:TheNightmanCodeth/x13s-nixos/jhovold-6.11";
+    x13s-nixos.url = "github:TheNightmanCodeth/x13s-nixos/jhovold-6.12-rc1";
     catppuccin.url = "github:catppuccin/nix";
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 
